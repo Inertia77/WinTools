1d2e21852cdb9056cf7edce7b6dec0967dc1fdd2
Inertia77 <miketsusama@gmail.com>
2026-07-25 17:41:16 +0900

    整理

---
+++ 通用工具/一键打开任务和工作台.bat
@@ -0,0 +1,20 @@
+@echo off
+chcp 65001 >nul
+title Open Website Collection
+
+:: ============================================================
+::  Open Website Collection
+::  Edit the list below: one "start" line per site.
+::  - Add a site:    start "" "https://example.com"
+::  - Disable a site: put :: in front of the line
+:: ============================================================
+
+:: start "" "https://www.google.com"
+:: start "" "https://github.com"
+:: start "" "https://mail.google.com"
+:: start "" "https://www.youtube.com"
+start "" "https://inertia77.github.io/task-ring/"
+start "" "https://inertia77.github.io/GUCC/"
+start "" "https://chatgpt.com/"
+
+exit
